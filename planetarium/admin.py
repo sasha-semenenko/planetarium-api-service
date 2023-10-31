23464b2f8c4d22406490fc46b092c06c490f915e
Sasha Semenenko <semenenko233249@gmail.com>
2023-10-31 21:32:46 +0200

    Register models in admin panel

--- planetarium/admin.py
+++ planetarium/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from planetarium.models import (
+    ShowTheme,
+    AstronomyShow,
+    ShowSession,
+    Ticket,
+    PlanetariumDome,
+    Reservation
+)
+
+admin.site.register(ShowTheme)
+admin.site.register(AstronomyShow)
+admin.site.register(ShowSession)
+admin.site.register(Ticket)
+admin.site.register(PlanetariumDome)
+admin.site.register(Reservation)
